f65fda572cbb2f77b74662933456c496c25743b9
Iliyan Vutov <vutoff@me.com>
2019-03-11 15:58:20 +0200

    Remove prefix list id option

--- bastion.tf
+++ bastion.tf
@@ -103,10 +103,9 @@ resource "aws_security_group" "bastion" {
   }
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-    prefix_list_ids = ["pl-12c4e678"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
